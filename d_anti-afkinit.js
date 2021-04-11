4b0b6084387832e2fb0c3edd86ceacbcacc3ab4c
s-dsds <sylvodsds@gmail.com>
2021-04-11 20:20:32 +0200

    fixes

--- d_anti-afkinit.js
+++ d_anti-afkinit.js
@@ -1,7 +1,7 @@
 AFK_HANDLER.init(window.WLROOM, {maxPlayers: CONFIG.max_players,});
 
 COMMAND_REGISTRY.add("afk", ["!afk #seconds#: number of seconds allowed for afk, `afk 0` disables it, 1/4th of #seconds# will be used as grace time (eg for 20sec, 5sec grace time)"], (player, num) => {
-    let n = num.trim();
+    let n = (typeof num=='undefined')?'':num.trim();
     if (n == "" || isNaN(n)) {
         announce("you need to provide a number as argument, type `!help afk` for help", player, 0xFFF0000);
         return false;
--- game_outqueue.js
+++ game_outqueue.js
@@ -52,7 +52,7 @@ class OutQueue {
         return this.currentOut
     }
     #hasPlayedTooMuch(player) {
-        return this.lastGames.reduce(g => g.score.filter(s => s.player.auth==player.auth)>0 )>=maxGames
+        return this.lastGames.filter(s => s.filter(g => g.team!=0 && g.player.auth==player.auth).length>0 ).length>=maxGames
     }
     
     getChangeSet() {
